e09b02a2927bb6c3b9b97bcb02249804539408a0
Edward Gonsalves <edwardgonsalves24@gmail.com>
2018-08-31 20:33:29 +0530

    Remove submodule

---
+++ Analysis-Of-Algorithms(AOA)/Dijkstra.c
@@ -0,0 +1,96 @@
+#include<stdio.h>
+#define MAX 5	
+#define INF 9999
+int dist[MAX];
+int min;
+int visit[MAX]={0};
+
+
+void dijkstra( int a[MAX][MAX])
+{
+	int node = 0;
+	int i,j;
+    
+    	dist[node] = 0;
+ 
+  
+  	for( i=0; i<MAX ; i++ )
+	{
+		visit[node]=1;
+		
+		for(j=0;j<MAX;j++)
+		{
+			if( a[node][j]!=INF )
+			{
+				if( (dist[node]+a[node][j]) < dist[j] )
+				{
+					dist[j] = dist[node]+a[node][j];
+				}
+			}
+		}
+
+		min=INF;
+		for(j=0;j<MAX;j++)
+		{
+			if( visit[j]!=1 && dist[j]<min)
+			{
+				min=dist[j];
+				node=j;
+			}
+		}
+	}
+
+	
+	for(i=0;i<MAX;i++)
+	{
+		printf("%c  ",(char)(i+65));
+		printf("%d\n", dist[i]);
+    }
+
+}
+
+
+int main()
+{
+    
+    	int a[MAX][MAX];
+	int i,j;
+
+
+	printf("Enter adjacency matrix: \n");
+	for(i=0;i<MAX;i++)
+	{
+		for(j=0;j<MAX;j++)
+		{
+			scanf("%d",&a[i][j]);
+			if(a[i][j]==0)
+				a[i][j]=INF;
+		}
+	}
+
+	for(i=0;i<MAX;i++)
+		dist[i]=INF;
+    
+	printf("\n");
+	dijkstra( a );
+	
+	return 0;
+}
+
+/*
+Output:
+
+Enter adjacency matrix: 
+0 10 0 5 0
+0 0 1 2 0
+0 0 0 0 4
+0 3 9 0 2
+7 0 6 0 0
+
+A  0
+B  8
+C  9
+D  5
+E  7
+
+*/
---
+++ Analysis-Of-Algorithms(AOA)/GraphColor.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+int n;
+int Colors;
+int C[100];
+int G[100][100];
+
+int place(int node, int color)
+{
+	int i;
+	for(i=0;i<n;i++)
+	{
+		if(G[node][i]!=0)
+		{
+			if(C[i]==color)
+				return 0;
+		}
+	}
+	return 1;
+}
+
+int putColor( int node )
+{
+	int i, flag;
+	for(i=0;i<Colors;i++)
+	{
+		if(place(node, i))   		  
+		{
+			C[node] = i;
+
+			if( node==(n-1) )			//Nth node
+				return 1;
+
+			flag = putColor(node+1);    //Next node
+
+			if(flag==1)
+				return 1;
+		}
+	}
+
+	if( i==Colors )   //Backtrack
+		return 0;
+	
+	return 1;		
+}
+
+int main()
+{
+	int i,j;
+	n=6;
+	Colors=3;
+	
+	printf("Enter adjacency matrix: \n");
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			scanf("%d",&G[i][j]);
+		}
+	}
+	
+	for(i=0;i<n;i++)
+		C[i]=-1;
+	
+	putColor(0);
+
+	printf("\nSolution:   ");
+	for(i=0;i<n;i++)
+		printf("%d ",C[i]);
+
+	return 0;
+}
